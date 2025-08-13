327e01d5866a50b7471922cc9dfadf0f460ac800
AliaksandrPatapenka <sspvitebsk@gmail.com>
2025-08-14 01:54:09 +0300

    Изменил название папки

---
+++ src/main/java/ru/stepik/p04_loopOperators/Task23.java
@@ -0,0 +1,31 @@
+/*
+Выполните задачу с использованием цикла do-while.
+Пользователь вводит произвольное целое число (оно может быть отрицательным).
+Найдите количество цифр в числе и сумму цифр.
+Выведите через пробел сначала количество, а потом сумму цифр.
+ */
+
+
+
+package ru.stepik.p04_loopOperators;
+
+import java.util.Scanner;
+
+public class Task23 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a < 0) a = -a;
+        int kol = 0;
+        int sum = 0;
+        //int number;
+        do {
+            int number = a % 10;
+            kol ++;
+            sum += number;
+            a = a / 10;
+            } while (a != 0);
+        System.out.println(kol + " " + sum);
+        }
+    }
+
